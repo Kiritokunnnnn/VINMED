3be489be9862f410c01132cd4830a436b310b29a
Kevin Tran <U2389162@unimail.hud.ac.uk>
2025-03-09 23:58:15 +0000

    design homepage header

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,27 @@
 import axios from '../axios';
 
-const handleLoginApi= (userEmail,userPassword)=>{
- return axios.post('/api/login',{email: userEmail,password: userPassword}); //email and password from server
+const handleLoginApi = (userEmail, userPassword) => {
+    return axios.post('/api/login', { email: userEmail, password: userPassword }); //email and password from server
 }
 
-export {handleLoginApi}
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+const createNewUserService = (data) => {
+    console.log("data from react", data)
+    return axios.post('/api/create-new-user', data)
+}
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    });
+}
+
+const editUserService = (inputData) => {
+    return axios.put('/api/edit-user', inputData);
+}
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService, editUserService }
